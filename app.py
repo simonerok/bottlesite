10017ce4ea00bc9904ef0bb47a832c2b0240c6b2
Santiago Donoso <santiagodonoso@gmail.com>
2024-04-07 10:21:30 +0200

    ...

---
+++ app.py
@@ -0,0 +1,24 @@
+from bottle import default_app, get, run, static_file
+
+# https://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@github.com/santiagodonoso/bottlesite.git
+
+##############################
+@get("/")
+def _():
+    return "x"
+
+
+##############################
+try:
+    import production
+    application = default_app()
+except:
+    run(host="0.0.0.0", port=80, debug=True, reloader=True, interval=0)
+
+
+
+
+
+
+
+
